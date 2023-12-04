e9557503d9e7eb1b7277d983e699ecf7241d6eda
Carol <carolinav.st@gmail.com>
2023-12-04 19:12:30 -0300

    incluindo função adicionar

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function adicionar () {
+    //recuperar valores: nome do produto, quantidade e valor
+
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+
+    //fazer alert para ver se recuperou corretamente
+    
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+    //necessário separar nome do preço que no html estão juntos
+
+
+    //calcular o preço, o nosso subtotal (preço x quantidade digitada)
+    //adicionar no carrinho
+    //atualizar o valor total da compra
+}
+
+function limpar () {
+
+
+}
