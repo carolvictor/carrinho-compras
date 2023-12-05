92b4326252ca4c888cd3ae597c4a90416b5407bc
Carol <carolinav.st@gmail.com>
2023-12-04 21:01:49 -0300

    Uso do split e inclusão do cálculo de preço

--- js/app.js
+++ js/app.js
@@ -1,20 +1,15 @@
 function adicionar () {
-    //recuperar valores: nome do produto, quantidade e valor
 
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
-
-    //fazer alert para ver se recuperou corretamente
-    
-    alert(nomeProduto.value);
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
 
-    //necessário separar nome do preço que no html estão juntos
-
-
-    //calcular o preço, o nosso subtotal (preço x quantidade digitada)
-    //adicionar no carrinho
-    //atualizar o valor total da compra
 }
 
 function limpar () {
